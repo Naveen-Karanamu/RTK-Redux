733e006fc7ce5c62e9fccc274994043089fa35e2
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-05-19 22:38:25 +0530

    Added: Main file

---
+++ index.js
@@ -0,0 +1,28 @@
+const store=require('./app/store')
+const cakeActions =require("./app/features/cake/cakeslice").cakeActions
+const icecreamActions=require('./app/features/icecream/icecreamSlice').icecreamActions
+const fetchUsers=require("./app/features/user/userSlice").fetchUsers
+
+console.log("Initial State",store.getState());
+
+const unsubscribe=store.subscribe(()=>{
+    // console.log("Updated State",store.getState());
+})
+
+store.dispatch(fetchUsers())
+
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(3))
+
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.restocked(3))
+
+
+
+// unsubscribe() 
