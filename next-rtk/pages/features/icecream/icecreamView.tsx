776318af6e9bc5b879ebcc4b4a51f76111d4e3f4
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-05-19 23:58:22 +0530

    Added: Icecreams View page

---
+++ next-rtk/pages/features/icecream/icecreamView.tsx
@@ -0,0 +1,13 @@
+const IcecreamView = () => {
+    return (
+      <>
+        <div>
+          <h1>Number of Icecreamss</h1>
+          <button>Order Icecreams</button>
+          <button>Add Icecreams</button>
+        </div>
+      </>
+    );
+  };
+  
+  export default IcecreamView
