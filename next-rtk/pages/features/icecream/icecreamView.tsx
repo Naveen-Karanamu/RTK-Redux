ac7e4360e16f03dcdfedc18a155339e632db9f54
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-05-20 00:36:39 +0530

    Updated: Icecream's state

--- next-rtk/pages/features/icecream/icecreamView.tsx
+++ next-rtk/pages/features/icecream/icecreamView.tsx
@@ -1,8 +1,12 @@
+import { useSelector } from "react-redux";
+
+
 const IcecreamView = () => {
+  const numbOfIcecreams=useSelector((state)=>state.icecream.numOfIcecreams)
     return (
       <>
         <div>
-          <h1>Number of Icecreamss</h1>
+          <h1>Number of Icecreamss : {numbOfIcecreams}</h1>
           <button>Order Icecreams</button>
           <button>Add Icecreams</button>
         </div>
