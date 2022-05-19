671b9c0d650d7e3e7510d4a634c6a2e15f79be9c
Naveen Kumar <karanamunaveen2002@gmail.com>
2022-05-19 23:57:59 +0530

    Added: Cakes View page

---
+++ next-rtk/pages/features/cake/cakeView.tsx
@@ -0,0 +1,13 @@
+const CakeView = () => {
+  return (
+    <>
+      <div>
+        <h1>Number of Cakes</h1>
+        <button>Order Cake</button>
+        <button>Add Cake</button>
+      </div>
+    </>
+  );
+};
+
+export default CakeView
